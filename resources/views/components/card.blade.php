ddb731d907f8f7b95bffd4704a21c10f1bce2b93
abifauzannn <mohammadabifauzan64@gmail.com>
2024-08-09 09:53:45 +0700

    dashboard labeling

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -4,17 +4,15 @@
     class="p-4 rounded-lg border border-gray-300 hover:bg-slate-50 transition duration-300 ease-in-out flex flex-col items-start text-left shadow-md">
     <div class="flex justify-between items-center w-full">
         <img src="{{ $icon }}" alt="" class="w-10 h-10" loading="lazy">
-        @if ($status === 'new')
-            <span class="bg-[#88F2C8] text-black text-xs font-bold px-2 py-1 rounded-full -mt-5">New</span>
+        @if ($status === 'baru')
+            <span class="bg-[#88F2C8] text-black text-xs font-bold px-2 py-1 rounded-full -mt-5">BARU</span>
         @endif
     </div>
-
     <div class="py-2 w-full text-left">
         <div class="text-gray-900 text-base font-bold font-inter leading-normal">
             {{ $title }}
         </div>
     </div>
-
     <div class="text-black text-xs font-normal font-inter leading-normal w-full text-left">
         {{ $description }}
     </div>
